763adfdf844367edccd0cf8ef14a6b8c383d83eb
Alex  Yu <alexyu478@gmail.com>
2025-01-27 16:40:51 -0800

    Finished Assignment 1
    Assignment 1 completed

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,9 @@
 window.addEventListener('load', loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log('Hello! My name is Alex Yu.'+
+          'I am a first year graduate student here at PSU' +
+          'studying Computer Science. Computers are my life and I love doing all things' +
+          'related to it. I also have way too many hobbies ranging from music, working ' +
+          'on cars, to building keyboards.');
 }
